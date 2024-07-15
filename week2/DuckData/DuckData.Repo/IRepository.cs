c38c180b25fd978a65b522a6f89df6b3504bcc53
Jean-Luc Quatremain <jlq5450@outlook.com>
2024-07-15 16:27:58 -0400

    Interfaces and serialization.

---
+++ week2/DuckData/DuckData.Repo/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using DuckData.Models;
+
+namespace DuckData.Repo
+{
+    // Agreement/contract
+    // terms - the things that both parties agree to
+    // like a template to be filled and used to your specifications
+    // "we" (the class that extends this interface) are agreeing to use all of the methods that are laid out in the interface
+    public interface IRepository
+    {
+        void ReadAndWriteWithFile(string path);
+        void StreamReaderReadLine(string path);
+        void StreamReaderReadToEnd(string path);
+        List<Duck> ReadDucksFromFile(string path);
+        void SaveDuck(Duck myDuck, string path);
+
+        void SaveAllDucks(List<Duck> duckList, string path);
+
+        List<Duck> LoadAllDucks(string path);
+    }
+}
