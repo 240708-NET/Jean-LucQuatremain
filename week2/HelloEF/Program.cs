fffcf5e9ace7289606a55dfd34b29954f3500902
Jean-Luc Quatremain <jlq5450@outlook.com>
2024-07-19 12:27:30 -0400

    Adding create and read methods to HelloEF.

--- week2/HelloEF/Program.cs
+++ week2/HelloEF/Program.cs
@@ -9,11 +9,9 @@ namespace HelloEF
         {
             Console.WriteLine("Hello Again!");
             Pet MyPet = new Pet(){Name = "Peter", Cuteness = 10, Chaos = 2, Species = "Rabbit"};
-            Console.WriteLine(MyPet.Speak());
 
             // string ConnectionString = File.ReadAllText("./connectionstring.txt");
-            
-            
+        
             // DbContextOptions<DataContext> ContextOptions = new DbContextOptionsBuilder<DataContext>().UseSqlServer(ConnectionString).Options;
             //DataContext Context = new DataContext(ContextOptions);
 
@@ -22,6 +20,47 @@ namespace HelloEF
                 DbContextOptionsBuilder<DataContext> ContextOptions = new DbContextOptionsBuilder<DataContext>().UseSqlServer(ConnectionString);
                 DataContext Context = new DataContext(ContextOptions.Options);
             */
+
+            // Pet MyPet = CreatePet(MyPet);
+            MyPet = GetPet(MyPet);
+
+            Console.WriteLine(MyPet.Speak());
+        }
+
+        public static Pet GetPet(Pet MyPet)
+        {
+            using (var context = new DataContext())
+            {
+                var found = 
+                    from p in context.Pets.ToList()
+                    where p.Name == MyPet.Name
+                        && p.Cuteness == MyPet.Cuteness
+                        && p.Chaos == MyPet.Chaos
+                        && p.Species == MyPet.Species
+                    select p;
+
+                return found.FirstOrDefault();
+            }
+        }
+
+        public static Pet GetPetById(int id)
+        {
+            using (var context = new DataContext())
+            {
+                Pet pet = context.Pets.Find(id);
+                return pet;
+            }
+        }
+
+        public static Pet CreatePet(Pet newPet)
+        {
+            using (var context = new DataContext())
+            {
+                context.Add(newPet);
+                context.SaveChanges();
+                
+                return GetPet(newPet);
+            }
         }
     }
 
