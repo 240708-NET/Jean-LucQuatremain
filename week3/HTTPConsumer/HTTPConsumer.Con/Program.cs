b2cf556c0c15986f9618c10081f2534a6092e87e
Jean-Luc Quatremain <jlq5450@outlook.com>
2024-07-23 14:04:42 -0400

    Morning demo follow along.

---
+++ week3/HTTPConsumer/HTTPConsumer.Con/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Net.Http;
+using HTTPConsumer.Models;
+using System.Text.Json;
+
+namespace HTTPConsumer.Con
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello again!");
+
+            string uri = "https://jsonplaceholder.typicode.com/posts/";
+
+            HttpClient client = new HttpClient();
+
+            string response = await client.GetStringAsync(uri);
+
+            // Console.WriteLine(response);
+
+            List<Post> postList = JsonSerializer.Deserialize<List<Post>>(response);
+        
+            foreach (var i in postList)
+                Console.WriteLine(i.id + ": " + i.title);
+        }
+    }
+}
---
+++ week3/HTTPConsumer/HTTPConsumer.Models/Post.cs
@@ -0,0 +1,12 @@
+﻿namespace HTTPConsumer.Models;
+
+public class Post
+{
+    // Fields
+    public int userId { get; set; }
+    public int id { get; set; }
+    public string title { get; set; }
+    public string body { get; set; }
+
+    
+}
