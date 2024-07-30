e336f45f3c9ee5c8d7e3534ad649c150e6632f40
Jean-Luc Quatremain <jlq5450@outlook.com>
2024-07-30 14:26:49 -0400

    Finalize updates to main.js.

--- week4/toDoApp/main.js
+++ week4/toDoApp/main.js
@@ -5,13 +5,24 @@ const form = document.querySelector('form');
 // Create output for the results
 const main = document.querySelector("main");
 const output = document.createElement("section");
+const unorderedList = document.createElement("ul");
+
 main.appendChild(output);
+
+if(ToDoList.length == 0)
+{
+    output.innerHTML = "No tasks yet!";
+}
+else
+{
+    output.appendChild(unorderedList);
+}
 
 form.addEventListener("submit", (e) => {
     e.preventDefault();
 
     const taskDescription = document.querySelector('#taskDescription').value;
-    const dueDate = document.querySelector('dueDate').value;
+    const dueDate = document.querySelector('#dueDate').value;
 
     let myTask = {
         description: taskDescription,
@@ -19,4 +30,11 @@ form.addEventListener("submit", (e) => {
     };
 
     ToDoList.push(myTask);
+
+    output.innerHTML ="";
+    ToDoList.forEach(task => {
+        const newTask = document.createElement("li");
+        newTask.innerHTML = `${task.description} due date: ${task.dueDate}`;
+        output.appendChild(newTask);
+    })
 })
